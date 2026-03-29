b70b78ac29a787d008f3422cdd222cca8a70305e
Lasse T. <127113991+lt20202122@users.noreply.github.com>
2026-03-29 22:00:55 +0200

    Update QuoteSectionLg.tsx

--- waldbaden/src/app/QuoteSectionLg.tsx
+++ waldbaden/src/app/QuoteSectionLg.tsx
@@ -2,7 +2,7 @@ export default function QuoteSectionLg({ playfair }: { playfair: any }) {
     return (
         <section className="max-w-7xl mx-auto py-24 px-0 relative max-quote:hidden w-full md:w-screen">
             <div className="grid grid-cols-2 bg-accent overflow-hidden">
-                <div className="p-8 md:p-16 flex items-center h-screen">
+                <div className="p-8 md:p-16 flex items-center">
                     <blockquote className={`text-white ${playfair.className} text-2xl sm:text-[28px] leading-9`}>
                         „Ich ging in die Wälder, weil ich bewusst leben wollte. Ich wollte das
                         Dasein auskosten. Ich wollte das Mark des Lebens einsaugen! Und alles
@@ -14,7 +14,7 @@ export default function QuoteSectionLg({ playfair }: { playfair: any }) {
                     </blockquote>
                 </div>
 
-                <div className="relative h-full">
+                <div className="relative aspect-4/3">
                     <img
                         src="/1650391244514.jpg"
                         loading="lazy"
